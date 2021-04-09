3e6043cf252ba310dd031525507dacf4c29753d4
Vincent Richitelli <vin.richitelli@gmail.com>
2021-04-09 00:52:47 -0400

    fix

--- server.js
+++ server.js
@@ -1,57 +1,21 @@
-const express = require('express');
+// Require Dependencies
+const express = require("express");
+const fs = require("fs");
 const path = require('path');
-var fs = require('fs');
 
+// Initialize express app
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
+// Setup data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
-//routes
+//Require routes file
+require('./routes/routes')(app);
 
-    //path for index.html's style sheet
-app.get('/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, '/public/assets/css/styles.css')));
-    //path for notes.html's associated javascript file
-app.get('/js/index.js', (req, res) => res.sendFile(path.join(__dirname, '/public/assets/js/index.js')));
-
-    //root route
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
-
-    //notes endpoint
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-
-    //get request for note list, stored in db.json
-app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')));
-
-    //post request to note list, db.json; assigns ID associated with non-zero position in array
-app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    const rawNotes = fs.readFileSync(path.join(__dirname, '/db/db.json'));
-    const notes = JSON.parse(rawNotes);
-    newNote.id = (notes.length + 1);
-    notes.push(newNote);
-    const response = JSON.stringify(notes);
-    fs.writeFileSync(path.join(__dirname, '/db/db.json'), response);
-    res.json(newNote);
-});
-
-    //removes note element with matching ID of request; reassigns ID based on new non-zero array position
-app.delete('/api/notes/:id', (req, res) => {
-    const rawId = req.params.id;
-    const id = parseInt(rawId);
-    const rawNotes = fs.readFileSync(path.join(__dirname, '/db/db.json'));
-    const notes = JSON.parse(rawNotes);
-    const rawOut = [];
-    for (let i = 0; i<notes.length; i++){
-        if(notes[i].id != id){
-            notes[i].id = rawOut.length + 1;
-            rawOut.push(notes[i]);
-        }
-    }
-    const response = JSON.stringify(rawOut);
-    fs.writeFileSync(path.join(__dirname, '/db/db.json'), response);
-    res.json(response);
-});
-
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+// Setup listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
